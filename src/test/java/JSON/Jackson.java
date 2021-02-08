ca07e97dcc15056f0e0cc8900349bfbc82612fe8
Moran, Thomas <Thomas.Moran@firstdata.com>
2021-02-08 19:08:15 +0000

    new json working

--- src/test/java/JSON/Jackson.java
+++ src/test/java/JSON/Jackson.java
@@ -1,6 +1,7 @@
 package JSON;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -8,24 +9,81 @@ import org.junit.jupiter.api.Test;
 public  class Jackson {
 
         @Test
-        public void JsonToJavaObject() throws JsonProcessingException {
+        public void JsonToJavaUtils() throws JsonProcessingException {
 
             // JSON string to Java Object  ... using readValue()
             ObjectMapper mapper = new ObjectMapper();
 
-            String s="{\"coord\":{\"lon\":-80.25,\"lat\":43.55},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"Sky is Clear\"}]}";
-            Object obj = mapper.readValue(s, Object.class );
+            String json ="["
+                + "  {"
+                + "    \"id\": 1,"
+                + "    \"name\": {"
+                + "      \"first\": \"Yong\","
+                + "      \"last\": \"Mook Kim\""
+                + "    },"
+                + "    \"contact\": ["
+                + "      {"
+                + "        \"type\": \"phone/home\","
+                + "        \"ref\": \"111-111-1234\""
+                + "      },"
+                + "      {"
+                + "        \"type\": \"phone/work\","
+                + "        \"ref\": \"222-222-2222\""
+                + "      }"
+                + "    ]"
+                + "  },"
+                + "  {"
+                + "    \"id\": 2,"
+                + "    \"name\": {"
+                + "      \"first\": \"Tom\","
+                + "      \"last\": \"Moran\""
+                + "    },"
+                + "    \"contact\": ["
+                + "      {"
+                + "        \"type\": \"phone/home\","
+                + "        \"ref\": \"333-333-1234\""
+                + "      },"
+                + "      {"
+                + "        \"type\": \"phone/work\","
+                + "        \"ref\": \"444-444-4444\""
+                + "      }"
+                + "    ]"
+                + "  }"
+                + "]";
+
+
+            Object obj = mapper.readValue(json, Object.class );
 
             System.out.println("THE OBJECT = "+ obj );
 
             // Java objects to JSON string - compact-print
             String jsonString = mapper.writeValueAsString(obj);
-            System.out.println(jsonString);
+            //System.out.println(jsonString);
 
             // Java objects to JSON string - pretty-print ( much better :) )
             String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
             System.out.println(jsonInString2);
 
+            JsonNode rootArray = mapper.readTree(json);
+
+            for (JsonNode root : rootArray) {
+
+                // Get id
+                long id = root.path("id").asLong();
+                System.out.println("id : " + id);
+
+                // Get Name
+                JsonNode nameNode = root.path("name");
+                if (!nameNode.isMissingNode()) {
+                    System.out.println(nameNode.path("first").asText());
+                }
+
+                JsonNode contactNode = root.path("contact");
+                if (contactNode.isArray()) {
+                    System.out.println("Is this " + contactNode+ " node an Array? "
+                        + contactNode.isArray());
+                }
+            }
         }
 }
 
