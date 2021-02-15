53d52439f0251a707631915510ee838d69ca7c2b
Moran, Thomas <Thomas.Moran@firstdata.com>
2021-02-15 16:57:42 +0000

    runnable lambda

--- src/test/java/Lambdas/LambdaBasics.java
+++ src/test/java/Lambdas/LambdaBasics.java
@@ -12,19 +12,57 @@ public class LambdaBasics{
     @Test
     public void test1() {
 
-        MyStringLambda hello = () -> log.info("Hello World!");
-        FooLambdaInterface fooLambdaInterface = () -> log.info("Hi");
+        MyStringLambda hello = () -> log.info("Hi from MyStringLambda!");
+        FooLambdaInterface fooLambdaInterface = () -> log.info("Hi from FooLambdaInterface");
 
         hello.logMeOut();
         fooLambdaInterface.perform();
+
+        FooLambdaInterface anonInnerClass = new FooLambdaInterface() {
+            @Override
+            public void perform() {
+                log.info("Hi from AnonInnerClass");
+            }
+        };
+
+        anonInnerClass.perform();
 
         /** Doesn't work **/
         //assertThat(aBlockOfCode.logMeOut()).isEqualTo("Hello World!");
 
         SquareLambda squareLambda = (int a) -> a * a;
         assertThat(squareLambda.squared(2)).isEqualTo(4);
+
+        //StringLengthLamba stringLengthLamba = (String s) -> s.length();
+        StringLengthLambda stringLengthLamba = s -> s.length();
+        log.info("length is: " + stringLengthLamba.getLength("A very long string"));
+
+        printLength(s -> s.length());
+
     }
 
+    @Test
+    public void runnableAsInnerClass() {
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log.info("Printed inside runnable");
+            }
+        });
+
+        myThread.run();
+    }
+
+    @Test
+    public void runnableAsLambda() {
+        Thread myThread = new Thread(() -> log.info("Printed inside lambda runnable"));
+        myThread.run();
+    }
+
+
+    public void printLength(StringLengthLambda lengthLamba) {
+        log.info(String.valueOf(lengthLamba.getLength("Howlongami")));
+    }
 
     interface MyStringLambda {
         void logMeOut();
@@ -32,6 +70,10 @@ public class LambdaBasics{
 
     interface SquareLambda {
         int squared(int x);
+    }
+
+    interface StringLengthLambda {
+        int getLength(String s);
     }
 
 }
